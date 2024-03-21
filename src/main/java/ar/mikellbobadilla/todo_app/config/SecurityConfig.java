38b800cb421959c0c374a29ba5b8d92f795ad0e0
Sting <bobadillasting@gmail.com>
2024-03-21 18:06:39 -0300

    Other commit

--- src/main/java/ar/mikellbobadilla/todo_app/config/SecurityConfig.java
+++ src/main/java/ar/mikellbobadilla/todo_app/config/SecurityConfig.java
@@ -5,11 +5,18 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Configuration
@@ -24,7 +31,8 @@ public class SecurityConfig {
 
     /* SpringSecurity 6.1 */
     http
-      .csrf(csrf -> csrf.disable())
+      .cors(Customizer.withDefaults())
+      .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(
         httpRequest -> {
           httpRequest.requestMatchers("/api/auth/**").permitAll();
@@ -40,4 +48,14 @@ public class SecurityConfig {
     return http.build();
   }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET"));
+        configuration.setAllowedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
